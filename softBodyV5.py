228c8dfc69604e56e59062a90bd145abdf9fc074
wartonbega <66480904+wartonbega@users.noreply.github.com>
2021-04-12 17:09:10 +0200

    Update and rename softBodyV4.py to softBodyV5.py
    Tous les outils de changement des constantes inrun

--- softBodyV5.py
+++ softBodyV5.py
@@ -56,7 +56,7 @@ def SommeForce(j):
 
 Points = [[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15, 16, 17, 18, 19], [20, 21, 22, 23, 24]]
 Points = []
-for i in range(30):
+for i in range(40):
     if i%5 == 0:
         Points.append([])
     Points[-1].append(i)
@@ -74,15 +74,19 @@ springMouse.L0 = 0
 springVoi = Spring()
 springHor = Spring()
 springHor.Ks = 15
-springHor.L0 = math.sqrt(2*(springVoi.L0**2))
+
 
 alpha = 0.1
-collisionBox = 5
-PositionDepart = 300
-Espacement = 30
+collisionBox = 2
+PositionDepart = 200
+Espacement = 40
 gravite = 2
-width = 500
+width = 700
 height = 500
+absorption = 10
+
+springVoi.L0 = Espacement
+springHor.L0 = math.sqrt(2*(springVoi.L0**2))
 
 def Voisin(Point, index, indexLigne):
     for i in Points:
@@ -117,16 +121,16 @@ def CheckCollision(point):
     y = P[point].y
     if x >= width or x+collisionBox >= width:
         P[point].x = width - collisionBox
-        P[point].vx = (P[point].vx*-1) / 4
+        P[point].vx = (P[point].vx*-1) / absorption
     if y+collisionBox >= height:
         P[point].y = height-collisionBox
-        P[point].vy = (P[point].vy*-1) / 4
+        P[point].vy = (P[point].vy*-1) / absorption
     if x <= 0:
         P[point].x = 0
-        P[point].vx = (P[point].vx*-1) / 4
+        P[point].vx = (P[point].vx*-1) / absorption
     if y <= 0:
         P[point].y = 0
-        P[point].vy = (P[point].vy*-1) / 4
+        P[point].vy = (P[point].vy*-1) / absorption
 Init()
 
 nbr = 0
@@ -135,6 +139,7 @@ def actu(nbr):
     can.delete('all')
     dt = 0.1
     if clicked:
+        P['MousePoint'].L0 = 0
         minDis = 100
         po = 0
         for i in Points:
@@ -144,7 +149,7 @@ def actu(nbr):
                 if dis < minDis:
                     minDis = dis
                     po = x
-        
+    
         P[po].Mouse = [0,0]
         P[po].Mouse[0], P[po].Mouse[1] = P['MousePoint'].x, P['MousePoint'].y
         can.create_line(P[po].x, P[po].y, P['MousePoint'].x, P['MousePoint'].y, fill = 'white')
@@ -177,6 +182,7 @@ def actu(nbr):
 
 import tkinter as tk
 clicked = False
+repuslif = True
 
 def click(event):
     global clicked
@@ -193,11 +199,65 @@ def unclick(event):
 def motion(event):
     if clicked:
         P['MousePoint'].x, P['MousePoint'].y = event.x, event.y
+        
+def changeGravity():
+    global gravite
+    gravite = float(GraviteSpinBox.get())
+    
+def changeL0():
+    springVoi.L0 = int(L0SpinBox.get())
+    springHor.L0 = math.sqrt(2 * springVoi.L0**2)
+
+def changeKs():
+    global var
+    springVoi.Ks = int(KsSpinBox.get())
+    springHor.Ks = int(KsSpinBox.get())
+
+def changeM():
+    for i in Points:
+        for x in i:
+            P[x].m = int(MSpinBox.get())
+
 window = tk.Tk()
 window.title('softBody')
+window.config(bg = '#2a2a2e')
 
-can = tk.Canvas(window, bg = '#192332', width = width, height = height)
-can.grid()
+LabelGravite = tk.LabelFrame(window, text = 'Constante de gravitée', bg = '#2a2a2e', fg = 'white', )
+LabelGravite.grid(column = 0, row = 1)
+GraviteSpinBox = tk.Spinbox(LabelGravite, from_ = 0, to_ = 10, command = changeGravity, bg = '#2a2a2e', fg = 'white', wrap = True)
+GraviteSpinBox.pack()
+GraviteSpinBox.delete(0, tk.END)
+GraviteSpinBox.insert(tk.END, str(gravite))
+#2a2a2e
+LabelL0 = tk.LabelFrame(window, text = 'Longueure au repos', bg = '#2a2a2e', fg = 'white')
+LabelL0.grid(column = 0, row = 2)
+L0SpinBox = tk.Spinbox(LabelL0, from_ = 10, to_ = 50, command = changeL0,bg = '#2a2a2e', fg = 'white', wrap = True)
+L0SpinBox.pack()
+L0SpinBox.delete(0, tk.END)
+L0SpinBox.insert(tk.END, str(springVoi.L0))
+
+LabelKs = tk.LabelFrame(window, text = "Constante de d'élasticitée", bg = '#2a2a2e', fg = 'white')
+LabelKs.grid(column = 0, row = 3)
+KsSpinBox = tk.Spinbox(LabelKs, from_ = 5, to_ = 85, command = changeKs,bg = '#2a2a2e', fg = 'white', wrap = True)
+KsSpinBox.pack()
+KsSpinBox.delete(0, tk.END)
+KsSpinBox.insert(tk.END, str(springVoi.Ks))
+
+LabelM = tk.LabelFrame(window, text = 'Masse des points', bg = '#2a2a2e', fg = 'white')
+LabelM.grid(column = 0, row = 4)
+MSpinBox = tk.Spinbox(LabelM, from_ = 5, to_ = 50, command = changeM,bg = '#2a2a2e', fg = 'white', wrap = True)
+MSpinBox.pack()
+MSpinBox.delete(0, tk.END)
+MSpinBox.insert(tk.END, str(P[0].m))
+
+var = f'''La souris permet d'atirer l'objet vers un point.
+Recommendation : ne pas faire tourner l'objet sur lui même\n quand il n'y as pas de gravitée.
+'''
+Label = tk.Label(window, text = var, bg = '#2a2a2e', fg = 'white')
+Label.grid(column = 0, row = 5)
+
+can = tk.Canvas(window, bg = '#192332', width = width, height = height, highlightthickness=1)
+can.grid(column = 0, row = 0)
 can.bind('<Button>', click)
 can.bind('<ButtonRelease>', unclick)
 can.bind('<Motion>', motion)
